ab75f20b36e7ac4a35922455da4465a7fcc65ffb
志谷優介 <yusuke.shiya@gmail.com>
2023-04-08 20:13:44 +0900

    vuexの導入

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    // ここに状態を定義します。
+  },
+  mutations: {
+    // ここに状態を変更するミューテーションを定義します。
+  },
+  actions: {
+    // ここに非同期アクションを定義します。
+  },
+  modules: {
+    // ここにサブモジュールを定義します。
+  }
+})
