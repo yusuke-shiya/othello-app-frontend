7bd3765279001fbac110a03dc6da26e9d82df009
志谷優介 <yusuke.shiya@gmail.com>
2023-04-09 02:33:31 +0900

    GoogleFontsを読み込む

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import Unfonts from 'unplugin-fonts/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Unfonts({
+      google: {
+        families: ['Noto Sans JP', 'Oswald']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
