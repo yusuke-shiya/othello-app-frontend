c94610d69d81240f35a4ec73eb85293e5321c4d3
志谷優介 <yusuke.shiya@gmail.com>
2023-04-09 02:04:24 +0900

    reset cssを読み込む

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <Messages />
   </div>
 </template>
-
+<style scoped>
+@import url(modern-css-reset/dist/reset.min.css);
+</style>
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Messages from './components/Messages.vue'
