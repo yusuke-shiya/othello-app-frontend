d3efb4a90a1327ad35fdd676f5741ea7c46920c0
志谷優介 <yusuke.shiya@gmail.com>
2023-04-10 01:25:53 +0900

    vitestの周辺ライブラリの追加と設定

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [Vue()],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
+})
