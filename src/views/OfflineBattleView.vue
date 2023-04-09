66a66e472ce9babf1a458582eb80a7e28c8c17ae
志谷優介 <yusuke.shiya@gmail.com>
2023-04-10 01:29:02 +0900

    ひとまずOfflineBattleView.vueを表示する

---
+++ src/views/OfflineBattleView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+import HomeNavigation from '@/components/HomeNavigation.vue'
+</script>
+
+<template>
+  <AppHeader />
+  <main>
+    <HomeNavigation />
+  </main>
+</template>
