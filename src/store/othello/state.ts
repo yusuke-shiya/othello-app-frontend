d4a311446c83e6e9d8a95ab170ca387bd4232ae4
志谷優介 <yusuke.shiya@gmail.com>
2023-04-13 00:16:46 +0900

    boardの初期化処理が不正確だったので修正

--- src/store/othello/state.ts
+++ src/store/othello/state.ts
@@ -1,7 +1,9 @@
 import type { OthelloState } from './types'
 
 export const initialState: OthelloState = {
-  board: Array(8).map(() => Array(8).fill(null)),
+  board: Array(8)
+    .fill(null)
+    .map(() => Array(8).fill(null)),
   currentPlayer: 'black',
   blackScore: 2,
   whiteScore: 2,
