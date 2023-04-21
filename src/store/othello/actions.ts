6e817ceeac047363f74e4853ae72458ba238ddd4
志谷優介 <yusuke.shiya@gmail.com>
2023-04-21 10:19:15 +0900

    actionsに石を置くputStoneを定義し、付随するヘルパー関数をutilityに定義

--- src/store/othello/actions.ts
+++ src/store/othello/actions.ts
@@ -1,3 +1,25 @@
-export const actions = {
-  // 必要に応じて非同期処理や複数のミューテーションを組み合わせるためのActionsを定義
+import type { ActionTree } from 'vuex'
+import type { OthelloState } from './types'
+import type { RootState } from '../index'
+import { isValidMove, flipStones, getScore } from './utility'
+
+export const actions: ActionTree<OthelloState, RootState> = {
+  putStone({ state, commit }, { row, column }) {
+    // 石を置くことができるかどうかをチェック
+    if (!isValidMove(state.board, row, column, state.currentPlayer)) return
+
+    // 石をひっくり返す
+    const updatedBoard = flipStones(state.board, row, column, state.currentPlayer)
+
+    // 新しいボードをセット
+    commit('setBoard', updatedBoard)
+
+    // 次のプレイヤーにターンを変更する
+    const nextPlayer = state.currentPlayer === 'black' ? 'white' : 'black'
+    commit('setCurrentPlayer', nextPlayer)
+
+    // スコアを更新する
+    const score = getScore(state)
+    commit('setScore', score)
+  }
 }
