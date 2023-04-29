7aeb2c92a21bf2e523d7fb89c754611be77f30bc
志谷優介 <yusuke.shiya@gmail.com>
2023-04-29 19:32:18 +0900

    バトルルームの作成をリクエストするapiのエンドポイントを変更

--- src/api/BattleRoomsApi.ts
+++ src/api/BattleRoomsApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1/othello/battle-rooms',
+  baseURL: import.meta.env.VITE_API_BASE_URL + '/api/v1/battle_rooms',
   headers: {
     'Content-Type': 'application/json'
   }
